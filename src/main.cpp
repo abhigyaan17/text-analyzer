ea136d780c90969c9c3202df51871fce2467a0de
Abhigyaan Sharma <abhigyaansharma17@gmail.com>
2026-04-19 17:27:48 +0530

    Added function for counting total words

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
+#include <unordered_map>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
 
+class textAnalyzer{
+    public:
+        unordered_map<string,int> mp;
+        int totalWords(){
+            ifstream file("../data/sample.txt");
+            
+            if (!file.is_open()) {
+                cout << "Error opening file" << endl;
+            }
+            string line;
+            string word;
+            int count=0;
+            while (getline(file, line)) {
+                istringstream iss(line);
+                while(iss>>word){
+                    count++;
+                }
+            }
+            file.close();
+            return count;
+        }
+};
+
 int main() {
-    ifstream file("../data/sample.txt");
-
-    if (!file.is_open()) {
-        cout << "Error opening file" << endl;
-        return 1;
-    }
-
-    string line;
-
-    while (getline(file, line)) {
-        cout << line << endl;
-    }
-
-    file.close();
-
+    textAnalyzer obj;
+    cout<<"Total Words : "<<obj.totalWords()<<endl;
     return 0;
 }
