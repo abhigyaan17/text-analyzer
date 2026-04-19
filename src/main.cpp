b3834d8a55bc794f5d2bc222cc526080e3f7efc5
Abhigyaan Sharma <abhigyaansharma17@gmail.com>
2026-04-19 18:01:29 +0530

    Added function to display unique words from file

--- src/main.cpp
+++ src/main.cpp
@@ -73,12 +73,35 @@ class textAnalyzer{
             cout<<mostFreqWord<<" : "<<maxFreq<<endl;
             file.close();
         }
+
+        void uniqueCount(){
+            ifstream file("../data/sample.txt");
+            
+            if (!file.is_open()) {
+                cout << "Error opening file." << endl;
+            }
+            string line;
+            string word;
+            while (getline(file, line)) {
+                istringstream iss(line);
+                while(iss>>word){
+                    mp[word]++;
+                }
+            }
+            int uniq_count=0;
+            for(auto it:mp){
+                if(it.second==1) uniq_count++;
+            }
+            cout<<"Total Unique words : "<<uniq_count<<endl;
+            file.close();
+        }
 };
 
 int main() {
     textAnalyzer obj;
     // cout<<"Total Words : "<<obj.totalWords()<<endl;
     // obj.wordsFreq();
-    obj.maxFreq();
+    // obj.maxFreq();
+    obj.uniqueCount();
     return 0;
 }
